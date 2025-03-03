7519734843c04004dd90e60d649ac475713883ba
jkaur207 <jkaur207@rrc.ca>
2025-03-02 18:58:04 -0600

    Added customer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,5 +13,9 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "customers#index"
+
+  # Additional routes for customers
+  get "customers/alphabetized", to: "customers#alphabetized"
+  get "customers/missing_email", to: "customers#missing_email"
 end
